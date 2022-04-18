1f9504fc4368cf490d2039a282b007cc6ed54634
Francisco <navasfran98@gmail.com>
2022-04-18 00:09:38 -0600

    guardar categoria, plantilla de mensajes

--- productos/views.py
+++ productos/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import CategoriaProducto
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 # Create your views here.
 
@@ -19,12 +20,15 @@ def guardarCategoria( request):
     nombreGet =request.GET["nombre"]
     #variables de request
 
-    categorias=CategoriaProducto.objects.get(Codigo=codigoGet)
+    categorias=CategoriaProducto.objects.filter(Codigo=codigoGet)
     if categorias:
         #return HttpResponse("codigo duplicado")
+        messages.error(request, 'No se ha podido guardar la categoria porque el codigo ya existe.', extra_tags='danger')
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     else:
         categoria = CategoriaProducto(Nombre=nombreGet,
                                       Codigo=codigoGet,
                                       Habilitada=True)
         categoria.save()
+        messages.success(request, 'categoria de producto agregada correctamente', extra_tags='success')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
