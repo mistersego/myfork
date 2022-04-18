903dd305094fa4695206f56b474e183de2038b9b
Francisco <navasfran98@gmail.com>
2022-04-17 23:17:19 -0600

    guardar tipo categoria

--- productos/urls.py
+++ productos/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("productos", views.productos, name="productos"),
-    path("categorias", views.categoriasProductos, name="categorias")
+    path("categorias", views.categoriasProductos, name="categorias"),
+    path("categorias/save", views.guardarCategoria, name="categorias-guardar")
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from .models import CategoriaProducto
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
 # Create your views here.
 
 def productos(request):
@@ -9,3 +12,19 @@ def categoriasProductos(request):
     categorias=CategoriaProducto.objects.all()
     context ={'categorias':categorias}
     return render(request, 'categorias/categorias.html', context)
+
+def guardarCategoria( request):
+    #variables de request
+    codigoGet =request.GET["codigo"]
+    nombreGet =request.GET["nombre"]
+    #variables de request
+
+    categorias=CategoriaProducto.objects.get(Codigo=codigoGet)
+    if categorias:
+        #return HttpResponse("codigo duplicado")
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        categoria = CategoriaProducto(Nombre=nombreGet,
+                                      Codigo=codigoGet,
+                                      Habilitada=True)
+        categoria.save()
