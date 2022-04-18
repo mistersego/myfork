e345697633c40fab750aa8cf9ccbab2b6d975217
Francisco <navasfran98@gmail.com>
2022-04-18 12:26:12 -0600

    habilitar, editar, tipos de productos

--- productos/urls.py
+++ productos/urls.py
@@ -3,5 +3,7 @@ from . import views
 urlpatterns = [
     path("productos", views.productos, name="productos"),
     path("categorias", views.categoriasProductos, name="categorias"),
-    path("categorias/save", views.guardarCategoria, name="categorias-guardar")
+    path("categorias/save", views.guardarCategoria, name="categorias-guardar"),
+    path('categorias/cambioestado/<int:id>/',views.cambioEstadoCategoriaProducto, name="categoria-habilitado"),
+    path('categorias/categoria/<int:id>/',views.obtenercategoriaProducto, name="categoria")
 ]
--- productos/views.py
+++ productos/views.py
@@ -3,6 +3,7 @@ from .models import CategoriaProducto
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib import messages
+from django.urls import reverse
 
 # Create your views here.
 
@@ -13,6 +14,23 @@ def categoriasProductos(request):
     categorias=CategoriaProducto.objects.all()
     context ={'categorias':categorias}
     return render(request, 'categorias/categorias.html', context)
+
+def cambioEstadoCategoriaProducto(request,  id):
+    categoria=CategoriaProducto.objects.get(pk=id)
+    if categoria.Habilitada==1:
+        categoria.Habilitada=0
+        categoria.save()
+    else:
+        categoria.Habilitada=1
+        categoria.save()
+
+    messages.success(request, 'El cambio de estado de la categoria se ha realizado correctamente', extra_tags='success')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def obtenercategoriaProducto(request,  id):
+    categoria=CategoriaProducto.objects.get(pk=id)
+    context ={'categoria':categoria}
+    return render(request, 'categorias/categoria-editar.html', context)
 
 def guardarCategoria( request):
     #variables de request
