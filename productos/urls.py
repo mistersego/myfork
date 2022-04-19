effd280af3da72376eab2afb3d7fae4ba1d4afeb
Francisco <navasfran98@gmail.com>
2022-04-18 19:23:24 -0600

    editar y eliminar una categoria

--- productos/urls.py
+++ productos/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path("categorias", views.categoriasProductos, name="categorias"),
     path("categorias/save", views.guardarCategoria, name="categorias-guardar"),
     path('categorias/cambioestado/<int:id>/',views.cambioEstadoCategoriaProducto, name="categoria-habilitado"),
-    path('categorias/categoria/<int:id>/',views.obtenercategoriaProducto, name="categoria")
+    path('categorias/categoria/<int:id>/',views.obtenercategoriaProducto, name="categoria"),
+    path("categorias/edit", views.editarCategoria, name="categorias-editar"),
+    path("categorias/delete/<int:id>/", views.eliminarCategoria, name="categorias-eliminar"),
+
 ]
--- productos/views.py
+++ productos/views.py
@@ -32,6 +32,31 @@ def obtenercategoriaProducto(request,  id):
     context ={'categoria':categoria}
     return render(request, 'categorias/categoria-editar.html', context)
 
+def eliminarCategoria(request, id):
+    categoria = CategoriaProducto.objects.get(Id_CategoriaProd=id)
+    categoria.delete()
+    messages.success(request, 'categoria de producto eliminada correctamente', extra_tags='danger')
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def editarCategoria(request):
+    #variables de request
+    codigoGet =request.GET["codigo"]
+    nombreGet =request.GET["nombre"]
+    idGet=request.GET["id"]
+    #variables de request
+    categorias=CategoriaProducto.objects.filter(Codigo=codigoGet).exclude(Id_CategoriaProd=idGet)
+    categoria=CategoriaProducto.objects.get(pk=idGet)
+    if categorias:
+        messages.error(request, 'No se ha podido actualizar la categoria porque el codigo ya existe.', extra_tags='warning')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        categoria.Nombre=nombreGet
+        categoria.Codigo=codigoGet
+        categoria.save()
+        messages.success(request, 'categoria de producto actualizada correctamente', extra_tags='success')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
 def guardarCategoria( request):
     #variables de request
     codigoGet =request.GET["codigo"]
