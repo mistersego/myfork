04b34ad200b5c7909f8135e3e068b99fcfe56d2b
Andres Castro <andres.castro2503@gmail.com>
2022-05-08 15:02:13 -0600

    CRUD tabla pro_marca

--- productos/urls.py
+++ productos/urls.py
@@ -8,5 +8,10 @@ urlpatterns = [
     path('categorias/categoria/<int:id>/',views.obtenercategoriaProducto, name="categoria"),
     path("categorias/edit", views.editarCategoria, name="categorias-editar"),
     path("categorias/delete/<int:id>/", views.eliminarCategoria, name="categorias-eliminar"),
-
+    #marca
+    path('marcas',views.marcas, name = "marcas"),
+    path('marcas/save',views.guardar_marca,name = "guardar_marca"),
+    path('marcas/delete/<int:id>/',views.eliminar_marca, name = "eliminar_marca"),
+    path('marcas/data/<int:id>/',views.actualizar_marca_datos, name = "actualizar_marca_datos"),
+    path('marcas/update',views.actualizar_marca, name = "actualizar_marca"),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
+
 from .models import CategoriaProducto
 from .models import Producto
+from .models import Marca
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
 from django.contrib import messages
 from django.urls import reverse
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 
@@ -87,3 +90,60 @@ def guardarCategoria( request):
         return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 #----------------------CATEGORIAS--------------------------------
+
+
+#------------------------MARCAS--------------------------------
+def marcas(request):
+    #marcas=Marca.objects.all()
+    marcas=Marca.objects.raw('SELECT m.id , m.marca, m.descripcion, COUNT(p.marca_id) as conteo FROM pro_marca AS m LEFT JOIN pro_producto p ON m.id = p.marca_id GROUP BY m.marca;')
+    context ={'marcas':marcas}
+    return render(request,'marcas/marcas.html', context)
+
+def guardar_marca(request):
+    nombre_marca = request.POST['marca']
+    descripcion = request.POST['descripcion']
+
+    marca = Marca.objects.filter(marca = nombre_marca)
+    if marca:
+        messages.error(request, 'Ya existe una marca con el nombre ingresado.', extra_tags='danger')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        marca_nueva = Marca(marca = nombre_marca, descripcion = descripcion)
+        marca_nueva.save()
+        messages.success(request, 'Marca agregada correctamente.', extra_tags='success')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def eliminar_marca(request, id):    
+    marca_productos = Producto.objects.filter(marca_id = id).count()
+    
+    if marca_productos > 0 :
+        messages.error(request, 'No puede eliminar la marca porque cuenta con '  +  str(marca_productos) + ' productos asignados.', extra_tags='danger')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        marca = Marca.objects.get(id = id)
+        marca.delete()
+        messages.success(request, 'Marca eliminada correctamente.', extra_tags='success')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    
+
+def actualizar_marca(request):
+    marca = get_object_or_404(Marca, id=request.POST['id'])
+    marcas = Marca.objects.filter(marca = request.POST['marca']).exclude(id = request.POST['id']).count()
+
+    if marcas > 0:
+        messages.success(request, 'Ya existe una marca con el nombre ingresado.', extra_tags='danger')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        marca.marca = request.POST['marca']
+        marca.descripcion = request.POST['descripcion']
+        marca.save()
+        
+        messages.success(request, 'Marca actualizada correctamente.', extra_tags='success')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+def actualizar_marca_datos(request,id):
+    marca = get_object_or_404(Marca, id=id)
+    json_response = JsonResponse({'id':marca.id,'marca':marca.marca,'descripcion':marca.descripcion})
+    return json_response
+    
+#------------------------MARCAS--------------------------------
